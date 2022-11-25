be5c8dcf64a42d585c674190ee35b90b27f442c9
sidratulshatil <105915771+sidratulshatil@users.noreply.github.com>
2022-11-26 00:02:16 +0600

    Handle Delete Add

--- src/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,9 +1,64 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const AllBuyers = () => {
+    const url = `http://localhost:5000/users`
+
+    const { data: users = [], refetch } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch(url)
+            const data = await res.json()
+            console.log(data)
+            return data
+        }
+    })
+    const deleteUser = (user) => {
+        fetch(`http://localhost:5000/users/${user}`, {
+            method: "DELETE",
+            // headers: {
+            //     authorization: `bearer ${localStorage.getItem('accessToken')}`
+            // }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.deletedCount > 0) {
+                    toast.success(`User deleted successfully`)
+                    refetch()
+                }
+
+            })
+
+    }
     return (
         <div>
-            <h2>All Buyers</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, i) => user.type === "Seller" && <tr key={user._id}>
+                                <th>{i + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.role !== "admin" && <button onClick={() => deleteUser(user._id)} className='btn btn-xs btn-warning text-white font-bold'>Delete</button>}</td>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/AllSellers/AllSellers.js
@@ -1,12 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
 const AllSellers = () => {
 
-
+    const [deletingUser, setDeletingUser] = useState(null)
     const url = `http://localhost:5000/users`
 
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await fetch(url)
@@ -15,7 +16,24 @@ const AllSellers = () => {
             return data
         }
     })
+    const deleteUser = (user) => {
+        fetch(`http://localhost:5000/users/${user}`, {
+            method: "DELETE",
+            // headers: {
+            //     authorization: `bearer ${localStorage.getItem('accessToken')}`
+            // }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.deletedCount > 0) {
+                    toast.success(`User deleted successfully`)
+                    refetch()
+                }
 
+            })
+
+    }
     return (
         <div>
             <div className="overflow-x-auto">
@@ -23,7 +41,7 @@ const AllSellers = () => {
 
                     <thead>
                         <tr>
-                            <th></th>
+                            <th>Serial</th>
                             <th>Name</th>
                             <th>Email</th>
                             <th>Delete</th>
@@ -35,7 +53,7 @@ const AllSellers = () => {
                                 <th>{i + 1}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
-                                <td>{user.role !== "admin" && <button className='btn btn-xs btn-warning text-white font-bold'>Delete</button>}</td>
+                                <td>{user.role !== "admin" && <button onClick={() => deleteUser(user._id)} className='btn btn-xs btn-warning text-white font-bold'>Delete</button>}</td>
                             </tr>)
                         }
 
